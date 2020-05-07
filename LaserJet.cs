c66260cd832aab9f0596bfb4224391c4c9955305
bagusawan <61877746+bagusawan@users.noreply.github.com>
2020-05-07 07:19:43 +0700

    Add files via upload

---
+++ LaserJet.cs
@@ -0,0 +1,13 @@
+using System;
+public class LaserJet : PrinterWindows
+    {
+        public override void show()
+        {
+         Console.WriteLine("Laserjet Display dimension : 12*12");
+        }
+
+        public override void print()
+        {
+         Console.WriteLine("LaserJet printer printing ...");
+        }
+    }
